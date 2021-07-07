748cfe04bb8993d2b243d6b9a0783329e2413841
Yang09701194 <mustnecessity@gmail.com>
2021-07-07 23:58:26 +0800

    HttpUtility

--- CSharpUtil/CPPCLIUtil/CPPCLIUtil.h
+++ CSharpUtil/CPPCLIUtil/CPPCLIUtil.h
@@ -1,10 +1,17 @@
 #pragma once
 
 using namespace System;
+using namespace System::Web;
 
 namespace CPPCLIUtil {
-	public ref class Class1
+	public ref class CPPCLIUtilCs
 	{
 		// TODO: Add your methods for this class here.
+		//  https://www.youtube.com/watch?v=xTRTY-fOIe8
+		public:
+			String^ UrlEncode2(String^ str)
+			{
+				return System::Web::HttpUtility::UrlEncode(str);
+			}
 	};
 }
