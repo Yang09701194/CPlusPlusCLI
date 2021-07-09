d7db1e0875184cc72a2db052ca032a3b859d9845
Yang09701194 <mustnecessity@gmail.com>
2021-07-09 19:15:37 +0800

    remove code

--- CSharpUtil/CPPCLIUtil/CPPCLIUtil.cpp
+++ CSharpUtil/CPPCLIUtil/CPPCLIUtil.cpp
@@ -1,31 +1,7 @@
 #include <stdafx.h>
 #include "CPPCLIUtil.h"
 
-//#include "ExampleObj.h"
 
-//using namespace CSharpLibrary;
-using namespace System;
-using namespace System::Runtime::InteropServices;
-
-void convertString(String^ str)
-{
-
-}
-
-void getStruct(Object^ obj, EventArgs^ args)
-{
-	
-}
-
-void evenNumberUpdate(Object^ obj, EventArgs^ args)
-{
-	
-}
-
-void oddNumberUpdate(Object^ obj, EventArgs^ args)
-{
-	
-}
 
 CPPCLIUTIL_DEC void example_print(char* message)
 {
--- CSharpUtil/CPPCLIUtil/CPPCLIUtil.h
+++ CSharpUtil/CPPCLIUtil/CPPCLIUtil.h
@@ -1,10 +1,6 @@
-
 #pragma once
-//
 //using namespace System;
 //using namespace System::Web;
-
-// ExampleObject.h
 
 #ifdef CPPCLIUTIL
 #define CPPCLIUTIL_DEC __declspec(dllexport)
@@ -20,28 +16,19 @@ extern "C"
 {
 #endif
 
-	CPPCLIUTIL_DEC typedef struct ExampleStruct
-	{
-		ExampleStruct() //default initializer
-			: Number(0), Message("") { }
+	//CPPCLIUTIL_DEC typedef struct ExampleStruct
+	//{
+	//	ExampleStruct() //default initializer
+	//		: Number(0), Message("") { }
 
-		int Number;
-		char* Message;
-	} ExampleStruct;
+	//	int Number;
+	//	char* Message;
+	//} ExampleStruct;
 
 	//Define int returns from C# library here. Allows to easily decode them in
-	//Native C/C++
-	const int EXAMPLE_PRINT_SUCCESS = 0;
-	const int EXAMPLE_PRINT_ERROR_NULLOREMPTY = 1;
-
+	//Native C/C++	
 	CPPCLIUTIL_DEC void example_print(char* message); // returns int, defined above
-	CPPCLIUTIL_DEC void example_add(int num);
-	CPPCLIUTIL_DEC void example_subtract(int num);
-
-	//Define functions that allow us to subscribe to C# events using callbacks
-	CPPCLIUTIL_DEC void example_subscribe_even(void(*funcPtr)(ExampleStruct));
-	CPPCLIUTIL_DEC void example_subscribe_odd(void(*funcPtr)(ExampleStruct));
-
+	
 #ifdef __cplusplus
 }
 #endif
