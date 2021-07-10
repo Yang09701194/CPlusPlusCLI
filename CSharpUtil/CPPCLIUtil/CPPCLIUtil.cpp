3472a753b23aa2ad34d362bfbfff16c99ab8db76
Yang09701194 <mustnecessity@gmail.com>
2021-07-10 11:08:10 +0800

    convert C++\CLI to C++

--- CSharpUtil/CPPCLIUtil/CPPCLIUtil.cpp
+++ CSharpUtil/CPPCLIUtil/CPPCLIUtil.cpp
@@ -1,9 +1,65 @@
-#include <stdafx.h>
+#include "stdafx.h"
+#include <vcclr.h>
+#include <string>
+#include <msclr\marshal_cppstd.h>
+//#include <msclr/marshal.h>
 #include "CPPCLIUtil.h"
+//
 
 
+using namespace CSharpUtil;
 
-CPPCLIUTIL_DEC void example_print(char* message)
+//using namespace System;
+//using namespace msclr::interop;
+
+gcroot<Helper^> helperObject = gcnew Helper();
+
+//  C++/CLI Part 1
+//  https://www.youtube.com/watch?v=xTRTY-fOIe8
+CPPCLIUTIL_DEC std::string UrlEncodeUtil(char* str)
 {
+	//String^ res =  helperObject->UrlEncodeUtil(gcnew String(str));
+
+	////  __declspec(dllexport) cannot accept return String^
+	////  will build error, so need to convert to std::string 
+	////return System::Web::HttpUtility::UrlEncode(str);
+
+	//std::string temp = marshal_as<std::string>(res);
+	//return temp;
+
+	std::string str2 = "Here";
+	String^ Str = gcnew String(str2.c_str());
+	String^ res  = System::Web::HttpUtility::UrlEncode(Str);
+
+	//https://stackoverflow.com/questions/12835924/export-c-function-from-a-c-cli-dll-that-uses-net-code
+	/*System::String^ result = "";
+	std::string unmanaged2 = msclr::interop::marshal_as<std::string>(result);
+	return unmanaged2;*/
+
+	//System::Runtime::InteropServices::Marshal::StringToCoTaskMemUni("")
+
 	
+	//strcpy(output, temp.c_str());
+
+	//msclr::interop::marshal_context context;
+	//char* strValueAsCString = context.marshal_as<char*>("test");
+	//// Probably bad
+	//char* ansiStr = strdup(strValueAsCString);
+
+	return "";
+	
+
 }
+
+//
+//System::String^ ToCliString(const char* s)
+//{
+//	System::String ^result = marshal_as<System::String^>(s);
+//	return result;
+//}
+//const char* ToCppString(System::String^ s)
+//{
+//	msclr::interop::marshal_context context;
+//	const char* result = context.marshal_as<const char*>(s);
+//	return result;
+//}
--- CSharpUtil/CPPCLIUtil/CPPCLIUtil.h
+++ CSharpUtil/CPPCLIUtil/CPPCLIUtil.h
@@ -1,13 +1,15 @@
-#pragma once
-//using namespace System;
-//using namespace System::Web;
+//#pragma once
+using namespace System;
+using namespace System::Web;
+
+//#include <string>
 
 #ifdef CPPCLIUTIL
 #define CPPCLIUTIL_DEC __declspec(dllexport)
 #else
 #define CPPCLIUTIL_DEC __declspec(dllimport)
 #endif
-
+//
 #ifndef CPPCLIUTIL_H
 #define CPPCLIUTIL_H
 
@@ -27,7 +29,7 @@ extern "C"
 
 	//Define int returns from C# library here. Allows to easily decode them in
 	//Native C/C++	
-	CPPCLIUTIL_DEC void example_print(char* message); // returns int, defined above
+	CPPCLIUTIL_DEC std::string UrlEncodeUtil(char* str); // returns int, defined above
 	
 #ifdef __cplusplus
 }
@@ -35,15 +37,15 @@ extern "C"
 
 #endif
 
-//
-//
+
 //namespace CPPCLIUtil {
 //	public ref class CPPCLIUtilCs
 //	{
 //		// TODO: Add your methods for this class here.
+//		// C++/CLI Part 1
 //		//  https://www.youtube.com/watch?v=xTRTY-fOIe8
 //		public:
-//			String^ UrlEncode2(String^ str)
+//			String^ UrlEncodeUtil(String^ str)
 //			{
 //				return System::Web::HttpUtility::UrlEncode(str);
 //			}
